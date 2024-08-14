968bf5bab9aa2450daf28482d766e180146eb693
김동우 <t6u80o@naver.com>
2024-08-14 15:02:42 +0900

    draggable 기능 추가

--- app/portfolio/components/DetailPromptContainer.tsx
+++ app/portfolio/components/DetailPromptContainer.tsx
@@ -2,45 +2,49 @@
 
 import { useFolderStore, useWindowStore } from "@/app/store";
 import MainFolderContainer from "./folderComponents/MainFolderContainer";
-import { Rnd } from "react-rnd";
+import Draggable from "react-draggable";
+import { useRef } from "react";
 
 export default function DetailPromptContainer({height} : {height ?: string}){
     const { isMinimized, isMaximized, isClosed, minimize, maximize, close } = useWindowStore();
     const { number } = useFolderStore();
+    const nodeRef = useRef(null);
 
     if (isClosed) return null; // 창이 닫혔을 때 아무것도 렌더링하지 않음
 
     return (
-        <div className={`window ${isMinimized ? 'minimized' : ''} ${isMaximized ? 'maximized' : ''}`}>
-            <div 
-                className="window-header"
-                onDoubleClick={() => {
-                    if(!isMaximized && isMinimized){
-                        maximize();
-                    }else if(!isMinimized && isMaximized){
-                        minimize();
-                    }
-                }}
-            >
-                <div className="window-buttons">
-                    <button className="minimize-button" onClick={minimize}></button>
-                    <button className="maximize-button" onClick={maximize}></button>
-                    <button className="close-button" onClick={close}></button>
+        <Draggable nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`window ${isMinimized ? 'minimized' : ''} ${isMaximized ? 'maximized' : ''}`}>
+                <div 
+                    className="window-header"
+                    onDoubleClick={() => {
+                        if(!isMaximized && isMinimized){
+                            maximize();
+                        }else if(!isMinimized && isMaximized){
+                            minimize();
+                        }
+                    }}
+                >
+                    <div className="window-buttons">
+                        <button className="minimize-button" onClick={minimize}></button>
+                        <button className="maximize-button" onClick={maximize}></button>
+                        <button className="close-button" onClick={close}></button>
+                    </div>
+                    <div className="window-title no-select">
+                        {
+                            number === 0?
+                            'About Me':
+                            number === 1?
+                            'Project':
+                            'All'
+                        }
+                    </div>
                 </div>
-                <div className="window-title no-select">
-                    {
-                        number === 0?
-                        'About Me':
-                        number === 1?
-                        'Project':
-                        'All'
-                    }
+                <div className="window-content text-black" style={{maxHeight : height}}>
+                    {/* 창 내용 */}
+                    <MainFolderContainer />
                 </div>
             </div>
-            <div className="window-content text-black" style={{maxHeight : height}}>
-                {/* 창 내용 */}
-                <MainFolderContainer />
-            </div>
-        </div>
+        </Draggable>
     );
 }
--- app/portfolio/components/FolderContainer.tsx
+++ app/portfolio/components/FolderContainer.tsx
@@ -1,50 +1,54 @@
 'use client';
 
 import { useFolderStore, useWindowStore } from '@/app/store';
-import { useState } from 'react';
-import { Rnd } from 'react-rnd';
+import { useRef, useState } from 'react';
+import Draggable from 'react-draggable';
 
 export default function FolderContainer(
     {folderName, index} : 
     {folderName :string, index : number}
 ){
 
-    const [isHovered, setIsHovered] = useState(false);
     const { setNumber } = useFolderStore();
-    const {isClosed, minimize} = useWindowStore();
+    const { isClosed, minimize } = useWindowStore();
+    const [isHovered, setIsHovered] = useState(false);
+
+    const nodeRef = useRef(null);
 
     return (
-        <div className='margin-container' style={{width : '80px', height : '120px', zIndex : 10}}>
-            <div 
-                className='folder-container' 
-                style={{ 
-                    width: '100%',
-                    zIndex : 1,
-                    position : 'relative'
-                }}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-                onDoubleClick={() => {
-                    setNumber(index); 
-                    if(isClosed){
-                        minimize();
-                    }
-                }}
-            >
-                <div className="row" style={{ margin: 'auto' }}>
-                    <div className="folder-tab col-3"></div>
-                    <div className="folder col-12"></div>
+        <Draggable nodeRef={nodeRef}>
+            <div ref={nodeRef} className='margin-container' style={{width : '80px', height : '120px', zIndex : 10}}>
+                <div 
+                    className='folder-container' 
+                    style={{ 
+                        width: '100%',
+                        zIndex : 1,
+                        position : 'relative'
+                    }}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                    onDoubleClick={() => {
+                        setNumber(index); 
+                        if(isClosed){
+                            minimize();
+                        }
+                    }}
+                >
+                    <div className="row" style={{ margin: 'auto' }}>
+                        <div className="folder-tab col-3"></div>
+                        <div className="folder col-12"></div>
+                    </div>
                 </div>
-            </div>
 
-            <p 
-                className='text-black text-center m-0' 
-                style={{
-                    zIndex : 10, 
-                    transition : 'all 0.2s', 
-                    opacity : isHovered ? 1 : 0
-                }}
-            >{folderName}</p>
-        </div>
+                <p 
+                    className='text-black text-center m-0' 
+                    style={{
+                        zIndex : 10, 
+                        transition : 'all 0.2s', 
+                        opacity : isHovered ? 1 : 0
+                    }}
+                >{folderName}</p>
+            </div>
+        </Draggable>
     );
 }
